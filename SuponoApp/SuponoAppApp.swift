727ce51813114e0482770b6f2a2d62f6e7be3e7c
Damian Diaz <deamdiaz27@gmail.com>
2024-12-26 17:13:53 +0100

    Initial Commit

---
+++ SuponoApp/SuponoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuponoAppApp.swift
+//  SuponoApp
+//
+//  Created by Deam on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SuponoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuponoAppTests/SuponoAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  SuponoAppTests.swift
+//  SuponoAppTests
+//
+//  Created by Deam on 26/12/24.
+//
+
+import Testing
+
+struct SuponoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
